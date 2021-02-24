0808f0f4c8b4e3ebdfacbfbadb57d2649f38237d
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-02-24 11:27:23 -0800

    Update styles for Header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./style.css";
 
 function Header() {
     return (
-        <div>
-            <h1>Employee Directory</h1>
+        <div className="jumbotron text-center header-container">
+            <h1>Employee Directory <i className="fas fa-briefcase"></i></h1>
             <p>Sort the employees by the name column, or further search for employees by using the search box!</p>
         </div>
     );
