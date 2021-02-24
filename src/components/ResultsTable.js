da0d5162bdaa1464c26bf1a4a162b7217bd2fccd
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-02-23 17:35:42 -0800

    Update to filter results using search box

--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function ResultsTable(props) {
-    console.log(props);
     return (
         <div className="container">
             <ul>
--- src/components/SearchEmployeeContainer.js
+++ src/components/SearchEmployeeContainer.js
@@ -3,9 +3,11 @@ import API from "../utils/API";
 import ResultsTable from "../components/ResultsTable"
 
 class SearchEmployeeContainer extends Component {
+    // 2 arrays: one for initial results given into "results"; the other for filtered results given into "newResults"
     state = {
         search: "",
-        results: []
+        results: [],
+        newResults: []
     };
 
     componentDidMount() {
@@ -14,17 +16,31 @@ class SearchEmployeeContainer extends Component {
 
     searchEmployees = () => {
         API.search()
-          .then(res => this.setState({ results: res.data.results }))
+          .then(res => this.setState({ results: res.data.results, newResults: res.data.results }))
           .catch(err => console.log(err));
     };
 
-    //Will need HandleInputChange function for search box
+    handleInputChange = event => {
+        event.preventDefault();
+        
+        let filteredResults = this.state.results.filter(employee => {
+            let employeeName = employee.name.first.toLowerCase();
+
+            return employeeName.includes(`${event.target.value}`);
+            
+        });
+
+        this.setState({
+            newResults: filteredResults
+        })
+
+    }
     
     render() {
         return (
             <div>
-                <input type="text" placeholder="Search Employee Name"/>
-                <ResultsTable results={this.state.results} />
+                <input type="text" placeholder="Search Employee Name" onChange={this.handleInputChange}/>
+                <ResultsTable results={this.state.newResults} />
             </div>
         )
     }
